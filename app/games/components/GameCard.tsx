bbb8dd5315c05ab0a548d9066e962e9eb7432b94
JheisonVR <daviid.vargasr@gmail.com>
2024-02-03 09:58:20 -0500

    Add react-icons library for game card icons

--- app/games/components/GameCard.tsx
+++ app/games/components/GameCard.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { Result } from "../interfaces/games-response";
 import Link from "next/link";
+import { CiCalendarDate } from "react-icons/ci";
+import { FaRegHeart } from "react-icons/fa";
+
 
 export const GameCard = ({ id, name, background_image, released }: Result) => {
   return (
@@ -17,37 +20,17 @@ export const GameCard = ({ id, name, background_image, released }: Result) => {
     // </div>
 
     <div className="mx-auto right-0 mt-2 w-60">
-      <div className="bg-white rounded overflow-hidden shadow-lg">
-        <div className="text-center p-6 bg-gray-800 border-b">
+      <div className="bg-white rounded-lg overflow-hidden shadow-lg">
+        <div className="flex flex-col justify-center gap-2 text-center p-5 bg-gray-800 border-b w-full h-56 overflow-hidden rounded-lg">
            <Image
-                className="h-50 w-50 text-white rounded-full mx-auto"
+                className="h-28 w-full text-white rounded-3xl mx-auto"
                 src={background_image}
                 alt={name}
-                width={200}
-                height={200}
+                width={300}
+                height={300}
                 
             />
-          {/* <svg
-            aria-hidden="true"
-            role="img"
-            className="h-24 w-24 text-white rounded-full mx-auto"
-            width="32"
-            height="32"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 256 256"
-          >
-            <path
-              fill="currentColor"
-              d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"
-            ></path>
-          </svg> */}
           <p className="pt-2 text-lg font-semibold text-gray-50">{name} </p>
-          <p className="text-sm text-gray-100">{released.toString()}</p>
-          <div className="mt-5">
-            {/* <a className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
-              Manage your Account
-            </a> */}
-          </div>
         </div>
         <div className="border-b">
           <Link
@@ -55,23 +38,13 @@ export const GameCard = ({ id, name, background_image, released }: Result) => {
             href="/account/campaigns"
           >
             <div className="text-green-600">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1"
-                viewBox="0 0 24 24"
-                className="w-5 h-5"
-              >
-                <path d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-              </svg>
+              <CiCalendarDate size={25} />
             </div>
             <div className="pl-3">
               <p className="text-sm font-medium text-gray-800 leading-none">
-                Campaigns
+              {released.toString()}
               </p>
-              <p className="text-xs text-gray-500">View your campaigns</p>
+              <p className="text-xs text-gray-500">Released date</p>
             </div>
           </Link>
           <Link
@@ -79,49 +52,16 @@ export const GameCard = ({ id, name, background_image, released }: Result) => {
             href="/account/donations"
           >
             <div className="text-gray-800">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1"
-                viewBox="0 0 24 24"
-                className="w-5 h-5"
-              >
-                <path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-              </svg>
+              <FaRegHeart size={25}/>
             </div>
             <div className="pl-3">
               <p className="text-sm font-medium text-gray-800 leading-none">
-                Donations
+                Favorite
               </p>
-              <p className="text-xs text-gray-500">View your last donations</p>
+              <p className="text-xs text-gray-500">Add to favorite</p>
             </div>
           </Link>
         </div>
-{/* 
-        <div className="">
-          <a href="#" className="w-full px-4 py-2 pb-4 hover:bg-gray-100 flex">
-            <p className="text-sm font-medium text-gray-800 leading-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="img"
-                className="h-4 w-4 text-gray-800 fill-current animate-spin"
-                width="32"
-                height="32"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  fill="currentColor"
-                  d="M988 548c-19.9 0-36-16.1-36-36c0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 0 0-94.3-139.9a437.71 437.71 0 0 0-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7c26.7 63.1 40.2 130.2 40.2 199.3c.1 19.9-16 36-35.9 36z"
-                ></path>
-              </svg>{" "}
-              Logout
-            </p>
-          </a>
-        </div> */}
       </div>
     </div>
   );
